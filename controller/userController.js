04448f374543c77c3ceec5a02ebc9b12af5a9c72
Lucas Simão <siimaoo.lucas@gmail.com>
2019-08-27 09:27:56 -0300

    Atualização userController

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,10 @@
 var bcrypt = require("bcryptjs");
-const mongoose = require("mongoose");
 const Users = require("../models/user");
+require('dotenv').config();
+
+const createUserToken = (userId) => {
+    return jwt.sign({id: userId}, process.env.SECRET_KEY , {expiresIn: '7d'});
+}
 
 module.exports = {
     async findUserById(req, res) {
@@ -23,6 +27,118 @@ module.exports = {
             })
         }
         catch (e) {
+            return res.send({
+                mensagem: "Ocorreu algum erro durante a requisição",
+                error: `Erro: \n ${e}`,
+                success: false
+            })
+        }
+    },
+    async createUser() {
+        try {
+            let { emai, nome, senha } = await req.body;
+            if (!email || !nome || !senha) {
+                return res.send({
+                    mensagem: "Preencha todos os campos!",
+                    error: `Erro: \n Algum campo não foi preenchido corretamente!`,
+                    success: false
+                })
+            }
+            else {
+                Users.findOne({ email: email }, (err, data) => {
+                    if (err) {
+                        return res.send({
+                            mensagem: "Ocorreu algum erro durante a requisição!",
+                            error: `Erro: \n ${err}`,
+                            success: false
+                        })
+                    }
+                    else if (data) {
+                        return res.send({
+                            mensagem: "Esse email já está em uso!",
+                            error: `Erro: \n ${data}`,
+                            success: false
+                        })
+                    }
+                    else {
+                        Users.create(req.body, (err, data) => {
+                            if (err) {
+                                return res.send({
+                                    mensagem: "Ocorreu algum erro durante a requisição!",
+                                    error: `Erro: \n ${err}`,
+                                    success: false
+                                })
+                            }
+                            else {
+                                data.senha = undefined;
+                                return res.send({
+                                    data,
+                                    token: createUserToken(data.id),
+                                    success: true
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+
+        } catch (e) {
+            return res.send({
+                mensagem: "Ocorreu algum erro durante a requisição",
+                error: `Erro: \n ${e}`,
+                success: false
+            })
+        }
+
+    },
+    async auth(req, res) {
+        try {
+            let { email, senha } = await req.body;
+            if (!email || !senha) {
+                return res.send({
+                    mensagem: "Preencha todos os campos!",
+                    error: `Erro: \n Algum campo não foi preenchido corretamente!`,
+                    success: false
+                })
+            }
+            else {
+                Users.findOne({ email: email }, (err, data) => {
+                    if (err) {
+                        return res.send({
+                            mensagem: "Ocorreu algum erro durante a requisição",
+                            error: `Erro: \n ${err}`,
+                            success: false
+                        })
+                    }
+                    else if (!data) {
+                        return res.send({
+                            mensagem: "Usuario não registrado",
+                            error: `Erro: \n Usuario não foi encontrado!`,
+                            success: false
+                        })
+                    }
+                    else {
+                        bcrypt.compare(senha, data.senha, (err, same) => {
+                            if (!same) {
+                                return res.send({
+                                    mensagem: "Senha incorreta!",
+                                    error: `Erro: \n A senha que foi digitada não confere!`,
+                                    success: false
+                                })
+                            }
+                            else {
+                                data.senha = undefined;
+                                return res.send({
+                                    data,
+                                    token: createUserToken(data.id),
+                                    success: true
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        } catch (e) {
             return res.send({
                 mensagem: "Ocorreu algum erro durante a requisição",
                 error: `Erro: \n ${e}`,
